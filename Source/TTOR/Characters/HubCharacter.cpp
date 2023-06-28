e46d6d3e87fae5617756097d933cf6c8e382a9a7
Martyna <martyna.plutowska@dziewczynywgrze.pl>
2023-06-28 17:41:55 +0100

    Hub Character Enhanced Input
    [git-p4: depot-paths = "//TTOR/": change = 81]

---
+++ Source/TTOR/Characters/HubCharacter.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HubCharacter.h"
+
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+// Sets default values
+AHubCharacter::AHubCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHubCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Get the player controller
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+
+	// Get Input Subsystem
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+
+	if (PlayerController && InputSubsystem)
+	{
+		InputSubsystem->AddMappingContext(InputContext, 0);
+	}
+}
+
+// Called every frame
+void AHubCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AHubCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AHubCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AHubCharacter::Look);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &AHubCharacter::StartJumping);
+	}
+}
+
+void AHubCharacter::Move(const FInputActionValue& Value)
+{
+	FVector2D MoveVector = Value.Get<FVector2D>();
+	FRotator YawRotation(0.f, GetControlRotation().Yaw, 0.f);
+	
+	// Forward Backward
+	FVector ForwardVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ForwardVector, MoveVector.Y);
+
+	// Left Right
+	FVector RightVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(RightVector, MoveVector.X);
+}
+
+void AHubCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D LookVector = Value.Get<FVector2D>();
+	AddControllerYawInput(LookVector.X);
+	AddControllerPitchInput(LookVector.Y);
+}
+
+void AHubCharacter::StartJumping(const FInputActionValue& Value)
+{
+	Jump();
+}
+
--- Source/TTOR/TTOR.Build.cs
+++ Source/TTOR/TTOR.Build.cs
@@ -8,7 +8,7 @@ public class TTOR : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 

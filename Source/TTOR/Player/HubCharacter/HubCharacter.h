0443a769680a41127004340c6f0832e882349625
Martyna <martyna.plutowska@dziewczynywgrze.pl>
2023-07-14 23:23:30 +0100

    Removes ThirdPerson folder; Removes vs plugin; New folder structure in Content folder and in source folder
    [git-p4: depot-paths = "//TTOR/": change = 91]

---
+++ Source/TTOR/Player/HubCharacter/HubCharacter.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+
+#include "HubCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class TTOR_API AHubCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AHubCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* InputContext;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveAction;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* LookAction;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* JumpAction;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void StartJumping(const FInputActionValue& Value);
+
+};
